e11c5fd551f08cc5eb0490e808bf79493959bc31
austindriggs <austind26@icloud.com>
2025-03-31 17:25:47 -0400

    [CHANGE] forgot to actually change AND stuff

--- MIPS_Translatron_3000/AND.c
+++ MIPS_Translatron_3000/AND.c
@@ -67,10 +67,10 @@ void and_reg_assm(void) {
 	setBits_str(5, "100100");
 
 	// set Rd
-	setBits_num(25, PARAM1.value, 5); // [CHANGE] PARAM1 should start at 15 (previously was 25)
+	setBits_num(15, PARAM1.value, 5); // [CHANGE] PARAM1 should start at 15 (previously was 25)
 
 	// set Rs
-	setBits_num(15, PARAM2.value, 5); // [CHANGE] PARAM2 should start at 25 (previously was 15)
+	setBits_num(25, PARAM2.value, 5); // [CHANGE] PARAM2 should start at 25 (previously was 15)
 
 	// set Rt
 	setBits_num(20, PARAM3.value, 5);
